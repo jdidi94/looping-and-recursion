6f2ddb5738786ce8b40874abca6aa340e699e5f2
jdidi94 <jdididaoud404@gmail.com>
2022-03-06 16:35:27 +0100

    adding exercices

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+//---------use recursion finish the below functions-----//
+function sum(x, y) {}
+// the second multiplying x,y
+function multiply(x, y) {}
+// the third divide numbers
+function divide(x, y) {}
+
 // exercise N°1
+
 //giving an input string return a same string but  remove all parentheses and replace it with commas ponctuation(','),the first letter should uppercased and   don't forget the full point in the end of paragraph (".");
 // if not a string return false
 // ponctuation('it"s morning),let"s hangout)===>'It"s morning,let"s hangout.'
@@ -6,5 +14,51 @@
 // make both methods  for loops and while loop
 
 function ponctuation() {}
-//count down the input number to 1 using recursion and for loop
+
+// exercise N°2:
+// count down the input number to 1
+//// make both methods  for loops and recursion
+
+// countDown(4)==>
+// 4
+// 3
+// 2
+// 1
+// countDown("hello")==>
+// 4
+// 3
+// 2
+// 1
+// the numbers range should be from -100 to 100
+
+//if it"s string  count the length and return the countdown
 function countDown(number) {}
+
+// exercise N°3:
+// program to find the factorial of a number
+// make it with recursion
+// factorial(3) returns 3 * factorial(2)
+// factorial(2) returns 3 * 2 * factorial(1)
+// factorial(1) returns 3 * 2 * 1 * factorial(0)
+// factorial(0) returns 3 * 2 * 1 * 1
+function factorial(x) {}
+
+// exercise N°4:
+
+// giving a string str and number n(number of time string should repeat) return the repeated  string n times:
+
+//  repeatString("dog",3)==>"dogdogdog"
+// repeatString("no",0)==>"give a number bigger than zero"
+
+function repeatString(str, n) {}
+//Advanced
+// exercise N°1
+// check the number is prime in giving range between x and y
+// Prime numbers between 25 and 50 are:
+// 29
+// 31
+// 37
+// 41
+// 43
+// 47
+function isPrime(x, y) {}
