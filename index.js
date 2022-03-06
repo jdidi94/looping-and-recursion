116630b9b4e8f64cf56a96cfde3b0ab106b68cf3
jdidi94 <jdididaoud404@gmail.com>
2022-03-06 16:34:06 +0100

    adding exercices

---
+++ index.js
@@ -0,0 +1,10 @@
+// exercise N°1
+//giving an input string return a same string but  remove all parentheses and replace it with commas ponctuation(','),the first letter should uppercased and   don't forget the full point in the end of paragraph (".");
+// if not a string return false
+// ponctuation('it"s morning),let"s hangout)===>'It"s morning,let"s hangout.'
+// ponctuation(231456),let's hangout)===>false*
+// make both methods  for loops and while loop
+
+function ponctuation() {}
+//count down the input number to 1 using recursion and for loop
+function countDown(number) {}
